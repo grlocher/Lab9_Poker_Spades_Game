bc248ab09d6e1895d2f57bdcbf2d4e09b0f16b36
Gabriel Locher <grlocher@gmail.com>
2024-05-28 20:52:15 -0400

    second commit

--- main.py
+++ main.py
@@ -22,18 +22,17 @@ card1 = rnd.choice(cards)
 card2 = rnd.choice(cards)
 card3 = rnd.choice(cards)
 
-card_order = ([card_values[card1], card_values[card2], card_values[card3]])
-
-
 def check_straight(card1, card2, card3):
+    card_order = ([card_values[card1], card_values[card2], card_values[card3]])
     card_order.sort()
     if card_order[1] == card_order[0] + 1 and card_order[2] == card_order[1] + 1:
-       return card_order[2]
+       return max(card_order)
     else:
         return 0
 
 
 def check_3ofa_kind(card1, card2, card3):
+    card_order = ([card_values[card1], card_values[card2], card_values[card3]])
     if card_order[0] == card_order[1] and card_order[0] == card_order[2]:
         return card_order[0]
     else:
@@ -41,16 +40,54 @@ def check_3ofa_kind(card1, card2, card3):
 
 
 def check_royal_flush(card1, card2, card3):
+    card_order = ([card_values[card1], card_values[card2], card_values[card3]])
+    card_order.sort()
     if check_straight(card1, card2, card3) == 14:
         return 14
     else:
         return 0
 
 
-# def play_cards(left1, left2, left3, right1, right2, right3):
-#     if left_total > right_total:
-#         return -1
-#     elif left_total == right_total:
-#         return 0
-#     elif left_total < right_total:
-#         return 1
+def play_cards(left1, left2, left3, right1, right2, right3):
+    # Types of hands
+    left_hand_straight = check_straight(left1, left2, left3)
+    left_hand_3ofa_kind = check_3ofa_kind(left1, left2, left3)
+    left_hand_royal_flush = check_royal_flush(left1, left2, left3)
+
+    right_hand_straight = check_straight(right1, right2, right3)
+    right_hand_3ofa_kind = check_3ofa_kind(right1, right2, right3)
+    right_hand_royal_flush = check_royal_flush(right1, right2, right3)
+
+    # Straight
+    if left_hand_straight and right_hand_straight:
+        if left_hand_straight > right_hand_straight:
+            return -1
+        elif left_hand_straight < right_hand_straight:
+            return 1
+        else:
+            return 0
+    elif left_hand_straight and not right_hand_straight:
+        return -1
+    elif not left_hand_straight and right_hand_straight:
+        return 1
+
+    # 3 of a kind
+    elif left_hand_3ofa_kind and right_hand_3ofa_kind:
+        if left_hand_3ofa_kind > right_hand_3ofa_kind:
+            return -1
+        elif left_hand_3ofa_kind < right_hand_3ofa_kind:
+            return 1
+        else:
+            return 0
+    elif left_hand_3ofa_kind and not right_hand_3ofa_kind:
+        return -1
+    elif not left_hand_3ofa_kind and right_hand_3ofa_kind:
+        return 1
+
+    # Royal Flush
+    elif left_hand_royal_flush and not right_hand_royal_flush:
+        return -1
+    elif not left_hand_royal_flush and right_hand_royal_flush:
+        return 1
+    else:
+        return 0
--- spades_unit_test.py
+++ spades_unit_test.py
@@ -1,10 +1,11 @@
 import unittest
-from main import check_straight, check_3ofa_kind, check_royal_flush
+from main import check_straight, check_3ofa_kind, check_royal_flush, play_cards
 
 
 class TestPokerSpadesGame(unittest.TestCase):
     def test_check_straight(self):
-        self.assertEqual(check_straight('S1', 'S2', 'S3'), 3)  # Checking sequential
+        self.assertEqual(check_straight('S2', 'S3', 'S4'), 4)  # Checking sequential
+        self.assertEqual(check_straight('S5', 'S6', 'S7'), 7)  # Checking another sequential
         self.assertEqual(check_straight('S10', 'SJ', 'SQ'), 12)  # Checking another sequential
         self.assertEqual(check_straight('S10', 'S5', 'S6'), 0)  # Checking non-straight
         self.assertEqual(check_straight('S7', 'S9', 'S8'), 9)  # Checking non-ordered straight
@@ -13,14 +14,29 @@ class TestPokerSpadesGame(unittest.TestCase):
         self.assertEqual(check_3ofa_kind('S9', 'S9', 'S9'), 9)  # Checking three of a kind
         self.assertEqual(check_3ofa_kind('SA', 'SA', 'SA'), 14)  # Checking Ace three of a kind
         self.assertEqual(check_3ofa_kind('S8', 'S10', 'S10'), 0)  # Checking non-three of a kind
-        self.assertEqual(check_3ofa_kind('S12', 'S3', 'S6'), 0)  # Checking non-three of a kind
+        self.assertEqual(check_3ofa_kind('S2', 'S3', 'S6'), 0)  # Checking non-three of a kind
 
     def test_check_royal_flush(self):
         self.assertEqual(check_royal_flush('SQ', 'SK', 'SA'), 14)  # Checking royal flush
         self.assertEqual(check_royal_flush('SA', 'SK', 'SQ'), 14)  # Checking royal flush, non-sequential
         self.assertEqual(check_royal_flush('SA', 'SK', 'SA'), 0)  # Checking non-royal flush with royals
-        self.assertEqual(check_royal_flush('S1', 'S2', 'S3'), 0)  # Checking sequential straight
-    # def test_play_cards(self):
-    #     self.assertEqual()
+        self.assertEqual(check_royal_flush('S2', 'S3', 'S4'), 0)  # Checking sequential straight
+
+    def test_play_cards(self):
+        # Straight
+        self.assertEqual(play_cards('S10', 'SJ', 'SQ', 'S8', 'S9', 'S10'), -1)  # Left winner
+        self.assertEqual(play_cards('S5', 'S6', 'S7', 'S6', 'S7', 'S8'), 1)  # Right winner
+        self.assertEqual(play_cards('S10', 'SJ', 'SQ', 'S10', 'SJ', 'SQ'), 0) # Draw
+
+        # 3 of a Kind
+        self.assertEqual(play_cards('S9', 'S9', 'S9', 'S8', 'S8', 'S8'), -1) # Left wins
+        self.assertEqual(play_cards('S9', 'S9', 'S9', 'SA', 'SA', 'SA'), 1) # Right winner
+
+        # Royal Flush
+        self.assertEqual(play_cards('SA', 'SK', 'SQ', 'S9', 'S10', 'SJ'), -1)  # Left winner
+        self.assertEqual(play_cards('S9', 'S10', 'SJ', 'SA', 'SK', 'SQ'), 1)  # Right winner
+        self.assertEqual(play_cards('SA', 'SK', 'SQ', 'SK', 'SQ', 'SA'), 0)  # Draw
+
+
 if __name__ == '__main__':
     unittest.main()
